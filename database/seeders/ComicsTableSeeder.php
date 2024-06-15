6e7cf9bc7933f268ab31a6b138d4ba7103e72fe7
Om12ek99 <omar.elkamili@gmail.com>
2024-06-15 18:22:40 +0200

    feat: added link to seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,16 +2,17 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        $comics = config('comics');
+
+        foreach ($comics as $comic) {
+            Comic::create($comic);
+        }
     }
 }
