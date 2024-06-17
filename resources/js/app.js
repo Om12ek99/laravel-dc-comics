1eca8f898c357bef8046c4317c30637ec93dbdb8
Om12ek99 <omar.elkamili@gmail.com>
2024-06-17 18:14:53 +0200

    add: conditional

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,4 +5,12 @@ import.meta.glob(["../img/**"]);
 
 document.addEventListener('DOMContentLoaded', function() {
     const deleteButton = document.getElementById('delete-button');
+    if (deleteButton) {
+        deleteButton.addEventListener('click', function(event) {
+            event.preventDefault();
+            if (confirm('Are you sure you want to delete this comic?')) {
+                deleteButton.closest('form').submit();
+            }
+        });
+    }
 });
