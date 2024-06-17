9b2008fc93e0a255f7f9b6fe04800bba0c7c3a51
Om12ek99 <omar.elkamili@gmail.com>
2024-06-17 18:13:13 +0200

    add: added event listener function

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,7 @@ import "./bootstrap";
 import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
