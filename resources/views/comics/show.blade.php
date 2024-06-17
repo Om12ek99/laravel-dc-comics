6af0c549d17ad76c274d89e9351124b061391e09
Om12ek99 <omar.elkamili@gmail.com>
2024-06-17 19:57:26 +0200

    add: style to the modal

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -21,24 +21,36 @@
         box-shadow: inset 0px 0px 20px 5px rgba(255, 255, 255, 0.05);
         backdrop-filter: blur(8px);
         -webkit-backdrop-filter: blur(8px);
-        width: 450px;
-        padding: 20px 30px;
-        border-radius: 10px;
+        width: 100%;
+        height: 100%;
         z-index: 1000;
         transition: top 0ms ease-in-out 0ms,
-                    opacity 300ms ease-in-out 0ms,
-                    transform 300ms ease-in-out 0ms;
+            opacity 300ms ease-in-out 0ms,
+            transform 300ms ease-in-out 0ms;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
-    .popup.active{
+
+    .popup.active {
         top: 50%;
         transform: translate(-50%, -50%, scale(1));
         opacity: 1;
         transition: top 0ms ease-in-out 0ms,
-                    opacity 300ms ease-in-out 0ms,
-                    transform 300ms ease-in-out 0ms;
+            opacity 300ms ease-in-out 0ms,
+            transform 300ms ease-in-out 0ms;
     }
 
-    .popup .close-btn{
+    .popup-container {
+        position: fixed;
+        width: 600px;
+        border: 1px solid rgba(255, 255, 255, 0.15);
+        padding: 20px 30px;
+        border-radius: 10px;
+        background: rgba(255, 255, 255, 0.7);
+    }
+
+    .popup .close-btn {
         color: black;
         position: absolute;
         top: 10px;
@@ -47,20 +59,19 @@
         height: 25px;
         font-size: 18px;
         text-align: center;
-        line-height: 25px;
-        background: rgba(255, 255, 255, 0.15);
+        line-height: 23px;
+        background: rgba(0, 0, 0, 0.2);
         border-radius: 50%;
         cursor: pointer;
 
     }
 
-    .popup h2{
+    .popup h2 {
         font-size: 35px;
-        text-align: center;
         margin: 10px 0px 20px;
     }
 
-    .popup p{
+    .popup p {
         font-size: 17px;
     }
 
@@ -68,9 +79,8 @@
         max-width: 80%;
         margin: 20px auto;
         padding: 20px;
-        border: 1px solid #ccc;
+
         border-radius: 5px;
-        background-color: #f9f9f9;
     }
 
     h1 {
@@ -116,53 +126,67 @@
     }
 </style>
 
-<div class="comic-details">
-    <div class="row">
-        <div class="col">
-            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+<div class="container">
+    <div class="comic-details">
+        <div class="row">
+            <div class="col">
+                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+            </div>
+            <div class="col">
+                <h1>{{ $comic->title }}</h1>
+                <p>{{ $comic->description }}</p>
+                <p>Price: ${{ $comic->price }}</p>
+                <p>Series: {{ $comic->series }}</p>
+                <p>Sale Date: {{ $comic->sale_date }}</p>
+                <p>Type: {{ $comic->type }}</p>
+            </div>
         </div>
-        <div class="col">
-            <h1>{{ $comic->title }}</h1>
-            <p>{{ $comic->description }}</p>
-            <p>Price: ${{ $comic->price }}</p>
-            <p>Series: {{ $comic->series }}</p>
-            <p>Sale Date: {{ $comic->sale_date }}</p>
-            <p>Type: {{ $comic->type }}</p>
+
+        <div class="actions d-flex">
+
+            <button id="open-popup" class="btn btn-danger">Delete</button>
+
+
+
+
+
+
+            <!-- link per editare il fumetto -->
+            <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-primary custom_edit">Edit</a>
+
+            <!-- form per aggiornare il fumetto -->
+
         </div>
+
+
+        <a href="{{ route('comics.index') }}">Back to list</a>
     </div>
-
-    <div class="actions d-flex">
-
-        <button id="open-popup" class="btn btn-danger">Delete</button>
-
-
-
-
-        
-
-        <!-- link per editare il fumetto -->
-        <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-primary custom_edit">Edit</a>
-
-        <!-- form per aggiornare il fumetto -->
-
-    </div>
-
-
-    <a href="{{ route('comics.index') }}">Back to list</a>
 </div>
 
+
 <div class="popup">
-    <div class="close-btn">
-        &times;
-    </div>
-    <h2>Sei sicuro di voler eliminare il fumetto corrente?</h2>
-    <p>l'azione non è piu reversibile</p>
-    <!-- elimina la voce dal database -->
-    <form id="delete-form" action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+    <div class="popup-container">
+        <div class="close-btn">
+            &times;
+        </div>
+        <div class="row">
+            <div class="col">
+            <img src="/images/danger.png" alt="Logo" style="height: 250px;">
+            </div>
+            <div class="col">
+                <h2>Sei sicuro di voler eliminare il fumetto corrente?</h2>
+                <p>l'azione non sarà piu reversibile</p>
+            </div>
+        </div>
+
+        <!-- elimina la voce dal database -->
+        <form id="delete-form" action="{{ route('comics.destroy', $comic->id) }}" method="POST">
             @csrf
             @method('DELETE')
             <button type="submit" class="btn btn-danger">Delete</button>
         </form>
+    </div>
+
 </div>
 
 <script>
