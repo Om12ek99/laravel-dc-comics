e92685a6637087ef5a8fd6d36791404b29caf411
Om12ek99 <omar.elkamili@gmail.com>
2024-06-17 18:29:01 +0200

    fix: items

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,3 @@ import "~resources/scss/app.scss";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
 
-document.addEventListener('DOMContentLoaded', function() {
-    const deleteButton = document.getElementById('delete-button');
-    if (deleteButton) {
-        deleteButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            if (confirm('Are you sure you want to delete this comic?')) {
-                deleteButton.closest('form').submit();
-            }
-        });
-    }
-});
