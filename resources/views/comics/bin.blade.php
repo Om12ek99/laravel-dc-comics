6da33f5c531681171515836693bdd6fb6d75dd47
Om12ek99 <omar.elkamili@gmail.com>
2024-06-18 17:49:21 +0200

    add:styling to buttons

--- resources/views/comics/bin.blade.php
+++ resources/views/comics/bin.blade.php
@@ -1,35 +1,37 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-        <h1 class="my-4">Cestino</h1>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Titolo</th>
-                    <th>Data Elimininazione</th>
-                    <th>Azioni</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($deletedcomics as $deletedComic)
-                    <tr>
-                        <td>{{ $deletedComic->title }}</td>
-                        <td>{{ $deletedComic->deleted_at }}</td> 
-                        <td>
-                        <form action="{{ route('comics.restore', $deletedComic->id) }}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <button type="submit" class="btn btn-success">Restore</button>
-                            </form>
-                            <form action="{{ route('comics.forceDelete', $deletedComic->id) }}" method="POST" style="display:inline-block;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>                       
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+    <h1 class="my-4">Cestino</h1>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Titolo</th>
+                <th>Data Elimininazione</th>
+                <th>Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($deletedcomics as $deletedComic)
+            <tr>
+                <td>{{ $deletedComic->title }}</td>
+                <td>{{ $deletedComic->deleted_at }}</td>
+                <td>
+                    <form action="{{ route('comics.restore', $deletedComic->id) }}" method="POST" style="display:inline-block;">
+                        @csrf
+                        @method('PATCH')
+                        <button type="submit" class="btn btn-success">Restore</button>
+                    </form>
+                    <form action="{{ route('comics.forceDelete', $deletedComic->id) }}" method="POST" style="display:inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+
 @endsection
