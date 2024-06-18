d671d7cded79faf4ffc165664480a49648c76363
Om12ek99 <omar.elkamili@gmail.com>
2024-06-18 18:09:34 +0200

    add:deleted previous file

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,3 +10,4 @@ document.querySelector("#open-popup").addEventListener("click", function() {
 document.querySelector(".popup .close-btn").addEventListener("click", function() {
     document.querySelector(".popup").classList.remove("active");
 });
+
