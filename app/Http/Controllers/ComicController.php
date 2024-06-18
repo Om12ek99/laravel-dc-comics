69c5db6cd41fb1104bfffce2628234cf8b2b5a3d
Om12ek99 <omar.elkamili@gmail.com>
2024-06-18 15:37:39 +0200

    feat: added italian to error display

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -55,20 +55,30 @@ class ComicController extends Controller
 
     // aggiorna
     public function update(Request $request, Comic $comic)
-    {
-        $validatedData = $request->validate([
-            'title' => 'required|string|max:255',
-            'description' => 'required|string',
-            'thumb' => 'required|string',
-            'price' => 'required|numeric',
-            'series' => 'required|string|max:255',
-            'sale_date' => 'required|date',
-            'type' => 'required|string|max:255',
-        ]);
+{
+    $validatedData = $request->validate([
+        'title' => 'required|string|max:255',
+        'description' => 'required|string',
+        'thumb' => 'required|string',
+        'price' => 'required|numeric',
+        'series' => 'required|string|max:255',
+        'sale_date' => 'required|date',
+        'type' => 'required|string|max:255',
+    ], [
+        'title.required' => 'Il campo titolo è obbligatorio.',
+        'description.required' => 'Il campo descrizione è obbligatorio.',
+        'thumb.required' => 'Il campo URL della miniatura è obbligatorio.',
+        'price.required' => 'Il campo prezzo è obbligatorio.',
+        'price.numeric' => 'Il campo prezzo deve essere un numero.',
+        'series.required' => 'Il campo serie è obbligatorio.',
+        'sale_date.required' => 'Il campo data di vendita è obbligatorio.',
+        'sale_date.date' => 'Il campo data di vendita deve essere una data valida.',
+        'type.required' => 'Il campo tipo è obbligatorio.'
+    ]);
 
-        $comic->update($validatedData);
-        return redirect()->route('comics.index');
-    }
+    $comic->update($validatedData);
+    return redirect()->route('comics.index');
+}
 
     // delete
     public function destroy(Comic $comic)
