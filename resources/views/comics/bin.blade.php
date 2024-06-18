0602da0b737c4857cbaa25bbb1cb7937bba4e6ca
Om12ek99 <omar.elkamili@gmail.com>
2024-06-18 17:27:32 +0200

    add: added layout that displays title and date of elimination

--- resources/views/comics/bin.blade.php
+++ resources/views/comics/bin.blade.php
@@ -1,8 +1,22 @@
 @extends('layouts.app')
 @section('content')
-<ul>
-    @foreach ($deletedcomics as $deletedComic)
-        <li>{{ $deletedComic->title }}</li>
-    @endforeach
-</ul>
+<div class="container">
+        <h1 class="my-4">Cestino</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Titolo</th>
+                    <th>Data Elimininazione</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($deletedcomics as $deletedComic)
+                    <tr>
+                        <td>{{ $deletedComic->title }}</td>
+                        <td>{{ $deletedComic->deleted_at }}</td>                        
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
